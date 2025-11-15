d257c2b827143ba534389860f9edb914bb5e918c
Loïc GUEZO <loicguezo@gmail.com>
2025-11-15 19:56:08 +0100

    feat(EBO): add EBO config

---
+++ inc/EBO.hpp
@@ -0,0 +1,18 @@
+#ifndef EBO_HPP
+#define EBO_HPP
+
+#include <GL/glew.h>
+
+class EBO
+{
+private:
+    GLuint id;
+
+public:
+    EBO(const unsigned int* indices, size_t size);
+    ~EBO();
+
+    void bind();
+    void unbind();
+};
+#endif
---
+++ src/EBO.cpp
@@ -0,0 +1,23 @@
+#include "EBO.hpp"
+
+EBO::EBO(const unsigned int* indices, size_t size)
+{
+    glGenBuffers(1, &id);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, id);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, size, indices, GL_STATIC_DRAW);
+}
+
+EBO::~EBO()
+{
+    glDeleteBuffers(1, &id);
+}
+
+void EBO::bind()
+{
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, id);
+}
+
+void EBO::unbind()
+{
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+}
