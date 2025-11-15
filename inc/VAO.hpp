8bd3080a84e5f69ef20e529870e52b98a4ae287c
Loïc GUEZO <loicguezo@gmail.com>
2025-11-15 20:01:08 +0100

    feat(VAO): add VAO config

---
+++ inc/VAO.hpp
@@ -0,0 +1,23 @@
+#ifndef VAO_HPP
+#define VAO_HPP
+
+#include <gl/glew.h>
+
+class VAO
+{
+private:
+    unsigned int id;
+
+public:
+    VAO();
+    ~VAO();
+
+    void bind();
+    void unbind();
+
+    void setAttributePointer(GLuint index, GLuint size, GLenum type, GLsizei stride, const void* offset);
+    void drawElement(GLenum mode, GLsizei count, GLenum type, const void *indices);
+    void drawArray(GLenum mode, GLint first, GLsizei count);
+};
+
+#endif
