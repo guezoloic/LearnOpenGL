bf782efa85ea51fb9424f6fc7b12a7597af22d61
Loïc GUEZO <loicguezo@gmail.com>
2026-01-07 13:50:49 +0100

    fix(vbo.cpp): add core namespace to functions

--- src/core/vbo.cpp
+++ src/core/vbo.cpp
@@ -2,7 +2,7 @@
 
 #include <cstddef>
 
-VBO::VBO(const void *data, std::size_t size)
+core::VBO::VBO(const void *data, std::size_t size)
 {
   // Generate a unique OpenGL buffer handle (ID).
   glGenBuffers(1, &id);
@@ -12,20 +12,20 @@ VBO::VBO(const void *data, std::size_t size)
   glBufferData(GL_ARRAY_BUFFER, size, data, GL_STATIC_DRAW);
 }
 
-VBO::~VBO()
+core::VBO::~VBO()
 {
   // delete the OpenGL buffer handle
   glDeleteBuffers(1, &this->id);
 }
 
-void VBO::bind()
+void core::VBO::bind()
 {
   // Bind this buffer to the GL_ARRAY_BUFFER target.
   // GL_ARRAY_BUFFER tells opengl this buffer contains vertices
   glBindBuffer(GL_ARRAY_BUFFER, this->id);
 }
 
-void VBO::unbind() 
+void core::VBO::unbind() 
 { 
   // bind the buffer to NULL handle (remove old id)
   glBindBuffer(GL_ARRAY_BUFFER, 0); 
